66069c4fcbd1e3198230b27eea894af4c327744d
Claudio Traspadini Oliveira <caltras@gmail.com>
2021-11-03 15:38:47 -0600

    Update error.handler.js

--- src/utility/error.handler.js
+++ src/utility/error.handler.js
@@ -1,8 +1,10 @@
+const http = require("http");
+
 module.exports = (res, err) => {
     let code = err.code;
 
     if (code){
-        code = err.code < 200 ? 500: err.code;
+        code = !http.STATUS_CODES[err.code]? 500: err.code;
     }
     res.status(code);
     res.send(err.message);
