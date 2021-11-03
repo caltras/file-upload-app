5758ba67f8c33cf83f19c43c9b0c786f8a6880a4
Claudio Traspadini Oliveira <caltras@gmail.com>
2021-11-02 18:17:36 -0600

    Added prod file

---
+++ src/config/application.prod.js
@@ -0,0 +1,31 @@
+module.exports = {
+    upload: {
+        destinationFolder: "/tmp/uploads",
+        accepts: [".tgz"],
+        maxSize: 25 * 1000 * 1000 * 1000
+    },
+    db: {
+        url: process.env.DB_HOST
+    },
+    log: {
+        level: "ERROR",
+        
+        // Use Elasticsearch as log provider
+        /*provider : {
+            name: "elasticsearch.logger.js",
+            host: "http://localhost:9200"    
+        }*/
+        
+        // Use console as log provider
+        /*provider:{
+            name: "console.logger.js"
+        },*/
+
+        // Use mongodb as log provider
+        provider : {
+            name: "mongodb.logger.js",
+            host: "mongodb://localhost:27017"
+        }
+        
+    }
+}
